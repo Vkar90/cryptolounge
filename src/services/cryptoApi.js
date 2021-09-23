6f2486edcffc6fc15d9162e808ec380f24b64dfe
Vkar90 <kardamitsis.e@gmail.com>
2021-09-23 10:47:28 +0300

    connect to the store with redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@redux.js/toolkit/query';
+
+const cryptoApiHeaders={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+     'x-rapidapi-key':'9f51dee89amsh123329558db31a3p12735bjsn273344a80ddd'
+}
+
+const baseUrl= 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
