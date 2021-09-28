8bb1a6b60b47e2744d2f7f2a955b20baab15d3ed
Vkar90 <kardamitsis.e@gmail.com>
2021-09-28 10:29:00 +0300

    set up crypto api

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '9f51dee89amsh123329558db31a3p12735bjsn273344a80ddd'
+  }
