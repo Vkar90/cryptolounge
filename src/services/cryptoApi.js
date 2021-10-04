d936692c61f108323b4156063422354eed7377e8
Vkar90 <kardamitsis.e@gmail.com>
2021-10-04 12:16:06 +0300

    add .env file

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,34 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoApiHeaders={
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-     'x-rapidapi-key':'9f51dee89amsh123329558db31a3p12735bjsn273344a80ddd'
-}
+const cryptoApiHeaders = {
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+};
 
-const baseUrl= 'https://coinranking1.p.rapidapi.com';
-
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`),
-        }),
-        getExchanges: builder.query({
-            query: (count) => createRequest(`/exchanges`),
-        }),
-        getCryptoDetails: builder.query({
-            query:(coinId) => createRequest(`/coin/${coinId}`),
-        }),
-        getCryptoHistory: builder.query({
-            query:({coinId, timeperiod }) => createRequest(`/coin/${coinId}/history/${timeperiod}`),
-        })
-    })
-})
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
+});
 
-export const {
-    useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
-} = cryptoApi;
-
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
